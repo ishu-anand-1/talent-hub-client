2b1cf5967f49d523d7cda072eb06a2a1f1eb0a81
ishu-anand-1 <talenthub.noreply@gmail.com>
2025-08-13 17:07:05 +0530

    Update CardGrid.jsx

--- client/src/components/CardGrid.jsx
+++ client/src/components/CardGrid.jsx
@@ -3,10 +3,10 @@ import { Music, Mic, Guitar, ListMusic } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 const cards = [
-  { title: "Dance", color: "from-pink-500 to-red-500", Icon: Music },
-  { title: "Singing", color: "from-blue-500 to-indigo-500", Icon: Mic },
-  { title: "Instrument", color: "from-green-500 to-emerald-500", Icon: Guitar },
-  { title: "Playlist", color: "from-purple-500 to-fuchsia-500", Icon: ListMusic },
+  { title: "Dance", color: "from-pink-500 to-red-500", glow: "shadow-pink-500/50", Icon: Music },
+  { title: "Singing", color: "from-blue-500 to-indigo-500", glow: "shadow-blue-500/50", Icon: Mic },
+  { title: "Instrument", color: "from-green-500 to-emerald-500", glow: "shadow-green-500/50", Icon: Guitar },
+  { title: "Playlist", color: "from-purple-500 to-fuchsia-500", glow: "shadow-purple-500/50", Icon: ListMusic },
 ];
 
 function CardGrid() {
@@ -14,15 +14,18 @@ function CardGrid() {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 p-10 mt-20">
-      {cards.map(({ title, color, Icon }, i) => (
+      {cards.map(({ title, color, glow, Icon }, i) => (
         <div
-           key={i}
+          key={i}
           onClick={() =>
             navigate(title === "Playlist" ? "/playlist" : `/videos/${title}`)
           }
-          className={`bg-gradient-to-br ${color} relative p-6 rounded-3xl text-white text-center transform transition duration-500 
-                      hover:scale-105 shadow-xl hover:shadow-2xl group cursor-pointer`}
-       style={{
+          className={`max-w-3xl min-h-[140px] lg:min-h-[150px] bg-white/95 p-6 lg:p-12 
+                      rounded-3xl border border-teal-100 shadow-md transition-transform duration-300 
+                      hover:shadow-lg lg:-rotate-1 lg:hover:rotate-0
+                      bg-gradient-to-br ${color} relative text-white text-center transform 
+                      hover:scale-105 shadow-xl hover:shadow-2xl hover:${glow} group cursor-pointer`}
+          style={{
             backdropFilter: "blur(10px)",
             WebkitBackdropFilter: "blur(10px)",
             border: "1px solid rgba(255, 255, 255, 0.1)",
@@ -35,7 +38,7 @@ function CardGrid() {
             <Icon className="w-8 h-8 text-white" />
           </div>
           <h2 className="text-lg font-extrabold tracking-wide">{title} Videos</h2>
-          <div className="absolute inset-0 rounded-3xl z-[-1] bg-white/5 blur-xl opacity-20 group-hover:opacity-40 transition" />
+          <div className="absolute inset-0 rounded-xl z-[-1] bg-white/5 blur-xl opacity-20 group-hover:opacity-40 transition" />
         </div>
       ))}
     </div>
